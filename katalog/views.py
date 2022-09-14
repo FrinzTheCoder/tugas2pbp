7e4391e2f6e58550786fb7c27a002b73834e0cf1
= <=>
2022-09-14 07:00:25 +0700

    second deployment

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+
+def show_katalog(request):
+
+    data_katalog = CatalogItem.objects.all()
+
+    context = {
+        'list_katalog': data_katalog,
+        'nama': 'Muhammad Falensi Azmi',
+        'npm': '2106630334',
+    }
+
+    return render(request, "katalog.html", context)
 
 # TODO: Create your views here.
